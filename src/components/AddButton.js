1f4de22509f6b9ebdf2a21e7f6db3ecdf363262e
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-07-07 19:21:29 +0700

    add creating new person

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -4,6 +4,28 @@ import { useState } from "react"
 
 const AddButton = () => {
     const [modalActive, setModalActive] = useState(false)
+    const [firstName, setfirstName] = useState('')
+    const [lastName, setlastName] = useState('')
+
+    const addPerson = async (e) => {
+        e.preventDefault()
+        if (!firstName || !lastName) {
+            return
+        }
+        const person = {
+            firstName: firstName,
+            lastName: lastName,
+        }
+        await fetch(`http://localhost:5000/persons/`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(person),
+        })
+        setfirstName('')
+        setlastName('')
+    }
     return (
         <div>
             <button className='user-list__add-btn' onClick={() => setModalActive(true)}>
@@ -11,12 +33,29 @@ const AddButton = () => {
             </button>
             <Modal active={modalActive} setActive={setModalActive}>
                 <div className='modal__header'>Создание сотрудника</div>
-                <button className='modal__back-link' onClick={() => setModalActive(false)}>Назад к списку</button>
-                <input className='modal__input' type='text' placeholder='Введите имя сотрудника'></input>
-                <input className='modal__input' type='text' placeholder='Введите фамилию сотрудника'></input>
-                <button className='user-list__add-btn modal__btn'>
-                    Сохранить
+                <button
+                    className='modal__back-link'
+                    onClick={() => setModalActive(false)}>
+                    Назад к списку
                 </button>
+                <form className='modal__form' onSubmit={addPerson}>
+                    <input
+                        className='modal__input'
+                        type='text'
+                        placeholder='Введите имя сотрудника'
+                        value={firstName}
+                        onChange={(e) => setfirstName(e.target.value)} />
+                    <input
+                        className='modal__input'
+                        type='text'
+                        placeholder='Введите фамилию сотрудника'
+                        value={lastName}
+                        onChange={(e) => setlastName(e.target.value)} />
+                    <input
+                        type='submit'
+                        value='Сохранить'
+                        className='user-list__add-btn modal__btn' />
+                </form>
             </Modal>
         </div >
 
